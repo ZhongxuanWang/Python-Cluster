4107f7d31c15093904e3474f409edd0e20e04e98
Daniel <cndanielwang@gmail.com>
2020-03-31 20:30:06 -0400

    bug FIX

--- NoRespond/NoRespond.py
+++ NoRespond/NoRespond.py
@@ -5,24 +5,28 @@ import sounddevice as sd
 
 import time
 
+time_duration = 0
+
 
 def wifi(order):
     os.system("networksetup -setairportpower airport " + str(order))
+    return
 
 
 def print_sound(indata, outdata, frames, time, status):
-    time_duration = 0
+    global time_duration
     try:
         volume_norm = int(np.linalg.norm(indata) * 10)
 
         if volume_norm < 7:  # Adjustable
             time_duration += 1
-            if time_duration > 160:  # Adjustable
-                wifi("on")
+            if time_duration > 300:  # Adjustable
+                os.system("networksetup -setairportpower airport off")
                 print("Wifi has been cut!")
-                time.sleep(3)  # Adjustable
-                wifi("off")
-                print("Wifi has been back to online!")
+                time_duration = 0
+                for a in range(10000000000): a += 1
+                os.system("networksetup -setairportpower airport on")
+                print(" - - - Wifi has been back to online!")
         else:
             print("|" * volume_norm)
             time_duration = 0
